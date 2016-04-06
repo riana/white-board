2d44248cf7eb01a6616d08d46e7c5840cbf11a40
riana <r.ralambo@gmail.com>
2016-04-06 21:14:15 +0200

    Load default data on startup and create its content if empty.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var mkdirp = require('mkdirp');
 var getDirName = require('path').dirname;
 console.log('user home dir : ' + os.homedir());
 
-var homeData = os.homedir();
+var storageDir = os.homedir() + "/.whiteboard-app/data/";
 
 function writeFile(path, contents, cb) {
 	mkdirp(getDirName(path), function (err) {
@@ -17,29 +17,39 @@ function writeFile(path, contents, cb) {
 }
 
 exports.enableDebug = function () {
-	homeData = ".";
+	storageDir = "./.whiteboard-app/data/";
 };
 
 exports.start = function (staticDir, internalPort) {
 	var express = require('express');
 	var bodyParser = require('body-parser')
 	var app = express();
+
+	console.log('storage dir : ', storageDir);
+
 	app.use('/', express.static(staticDir));
 	app.use(bodyParser.json());
 
 	app.get('/api/load', function (req, res) {
 		var id = req.query.id;
-		var filename = homeData + "/.whiteboard-app/data/" + id + ".json";
+		var filename = storageDir + id + ".json";
+		console.log("loading : ", filename);
 		fs.readFile(filename, 'utf8', function (err, data) {
-			// console.log(data);
-			res.end(data);
+			if (err && err.code === "ENOENT") {
+				res.end(JSON.stringify({
+					error: "Data not found"
+				}));
+			} else {
+				res.end(data);
+			}
+
 		});
 	});
 
 	app.post('/api/save', function (req, res) {
 		var id = req.query.id;
 		// console.log(req.query);
-		var outputFilename = homeData + "/.whiteboard-app/data/" + id + ".json";
+		var outputFilename = storageDir + id + ".json";
 		var data = req.body;
 		writeFile(outputFilename, JSON.stringify(data, null, 4), function (err) {
 			if (err) {
