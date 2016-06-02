57f8657d95d15807eca33c0b2ffc0633b36bc2d6
riana <r.ralambo@gmail.com>
2016-06-02 22:46:46 +0200

    Code cleanup. Remove warnings.

--- elements/utils/AppContext.js
+++ elements/utils/AppContext.js
@@ -2,29 +2,29 @@
 
 AppContext = {
 
-  constructor() {
-    this.globals = [];
-    this.listeners = [];
-  }
+	constructor() {
+		this.globals = [];
+		this.listeners = [];
+	},
 
-  listen(key, cb) {
-    if (!this.listeners[key]) {
-     this.listeners[key] = [];
-    }
-    this.listeners[key].push(cb);
-  }
+	listen(key, cb) {
+		if (!this.listeners[key]) {
+			this.listeners[key] = [];
+		}
+		this.listeners[key].push(cb);
+	},
 
-  getValue(key) {
-    return this.globals[key];
-  }
+	getValue(key) {
+		return this.globals[key];
+	},
 
-  setValue(key, value) {
-    let oldValue = this.globals[key];
-    this.globals[key] = value;
-    for (let listener of this.listeners[key]) {
-     listener(value, oldValue);
-    }
-  }
-}
+	setValue(key, value) {
+		let oldValue = this.globals[key];
+		this.globals[key] = value;
+		for (let listener of this.listeners[key]) {
+			listener(value, oldValue);
+		}
+	}
+};
 
 window.AppContext = window.AppContext || AppContext;
